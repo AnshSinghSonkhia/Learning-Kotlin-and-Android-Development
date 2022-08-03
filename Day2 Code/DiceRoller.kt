21d334fcf6c0d24e15d7b5d6b2a36758061a37e5
Ansh Singh Sonkhia <110414565+AnshSinghSonkhia@users.noreply.github.com>
2022-08-03 22:37:24 +0530

    Create DiceRoller.kt

---
+++ Day2 Code/DiceRoller.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val myFirstDice = Dice(6)
+    println("Your ${myFirstDice.numSides} sided dice rolled ${myFirstDice.roll()}!")
+    
+    val mySecondDice = Dice(20)
+    println("Your ${mySecondDice.numSides} sided dice rolled ${mySecondDice.roll()}!")
+}
+
+class Dice (val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
