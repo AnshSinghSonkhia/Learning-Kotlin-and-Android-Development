16983073815399ee2a681542b3c1da3a62cad519
Ansh Singh Sonkhia <110414565+AnshSinghSonkhia@users.noreply.github.com>
2022-08-05 18:02:26 +0530

    Update Classes-And-Inheritance-in-Kotlin.kt

--- Day4 Code/Classes-And-Inheritance-in-Kotlin.kt
+++ Day4 Code/Classes-And-Inheritance-in-Kotlin.kt
@@ -1,22 +1,23 @@
 fun main() {
+    
     val squareCabin = SquareCabin(6)
     val roundHut = RoundHut(3)
     val roundTower = RoundTower(4)
-       
+
     with(squareCabin) {
         println("\nSquare Cabin\n============")
         println("Capacity: ${capacity}")
         println("Material: ${buildingMaterial}")
         println("Has room? ${hasRoom()}")
     }
-    
+       
     with(roundHut) {
         println("\nRound Hut\n=========")
         println("Material: ${buildingMaterial}")
         println("Capacity: ${capacity}")
         println("Has room? ${hasRoom()}")
     }
-    
+       
     with(roundTower) {
         println("\nRound Tower\n==========")
         println("Material: ${buildingMaterial}")
@@ -35,17 +36,23 @@ abstract class Dwelling(private var residents: Int) {
    }
 }
 
+
 class SquareCabin(residents: Int) : Dwelling(residents) {
     override val buildingMaterial = "Wood"
     override val capacity = 6
 }
 
+
 open class RoundHut(residents: Int) : Dwelling(residents) {
-   override val buildingMaterial = "Straw"
-   override val capacity = 4
+    override val buildingMaterial = "Straw"
+    override val capacity = 4
 }
 
-class RoundTower(residents: Int) : RoundHut(residents) {
+
+class RoundTower(
+    residents: Int,
+    val floors: Int = 2) : RoundHut(residents) {
+
     override val buildingMaterial = "Stone"
-    override val capacity = 4
+    override val capacity = 4 * floors
 }
