fa21b38d3cd264e73744dabbb1df3c76769fed3c
Ansh Singh Sonkhia <110414565+AnshSinghSonkhia@users.noreply.github.com>
2022-08-05 17:58:34 +0530

    Update Classes-And-Inheritance-in-Kotlin.kt

--- Day4 Code/Classes-And-Inheritance-in-Kotlin.kt
+++ Day4 Code/Classes-And-Inheritance-in-Kotlin.kt
@@ -1,5 +1,8 @@
 fun main() {
     val squareCabin = SquareCabin(6)
+    val roundHut = RoundHut(3)
+    val roundTower = RoundTower(4)
+       
     with(squareCabin) {
         println("\nSquare Cabin\n============")
         println("Capacity: ${capacity}")
@@ -7,23 +10,29 @@ fun main() {
         println("Has room? ${hasRoom()}")
     }
     
-    val roundHut = RoundHut(3)
     with(roundHut) {
         println("\nRound Hut\n=========")
-    	println("Material: ${buildingMaterial}")
-    	println("Capacity: ${capacity}")
-    	println("Has room? ${hasRoom()}")
+        println("Material: ${buildingMaterial}")
+        println("Capacity: ${capacity}")
+        println("Has room? ${hasRoom()}")
     }
     
+    with(roundTower) {
+        println("\nRound Tower\n==========")
+        println("Material: ${buildingMaterial}")
+        println("Capacity: ${capacity}")
+        println("Has room? ${hasRoom()}")
+    }
 }
 
-abstract class Dwelling(private var residents: Int){
+
+abstract class Dwelling(private var residents: Int) {
     abstract val buildingMaterial: String
     abstract val capacity: Int
-    
+       
     fun hasRoom(): Boolean {
-    return residents < capacity
-    }
+       return residents < capacity
+   }
 }
 
 class SquareCabin(residents: Int) : Dwelling(residents) {
@@ -31,16 +40,12 @@ class SquareCabin(residents: Int) : Dwelling(residents) {
     override val capacity = 6
 }
 
-class RoundHut(residents: Int) : Dwelling(residents) {
-    override val buildingMaterial = "Straw"
-    override val capacity = 4
+open class RoundHut(residents: Int) : Dwelling(residents) {
+   override val buildingMaterial = "Straw"
+   override val capacity = 4
 }
 
 class RoundTower(residents: Int) : RoundHut(residents) {
-    /**
-     *  By default, in Kotlin, classes are final and cannot be subclassed. You are only allowed to inherit from abstract classes or classes that are marked with the open keyword. Hence you need to mark the RoundHut class with the open keyword to allow it to be inherited from.package
-     **/ 
-    
     override val buildingMaterial = "Stone"
     override val capacity = 4
 }
